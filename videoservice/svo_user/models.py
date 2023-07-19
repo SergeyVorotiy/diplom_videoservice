7b794a4bb8b7a2ea5bb165f351e49a3faa68684b
Sergey <vorotiy.sergey@yandex.ru>
2023-07-19 13:11:38 +0300

    updating models field

--- videoservice/svo_user/models.py
+++ videoservice/svo_user/models.py
@@ -1,3 +1,4 @@
+# Configuring custom user for video service app
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
--- videoservice/svo_videoserviceapp/models.py
+++ videoservice/svo_videoserviceapp/models.py
@@ -1,31 +1,33 @@
+# configuring models for video service app
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 from videoservice.settings import AUTH_USER_MODEL
 
 
-# Create your models here.
 class VSUser(models.Model):
     user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
-    black_list = ArrayField(models.CharField(max_length=128), blank=True, default=list())
-    ignore_list = ArrayField(models.CharField(max_length=128), blank=True, default=list())
-    subscribes = ArrayField(models.CharField(max_length=128), blank=True, default=list())
+    black_list = ArrayField(models.CharField(max_length=128), blank=True, default=list)
+    ignore_list = ArrayField(models.CharField(max_length=128), blank=True, default=list)
+    subscribes = ArrayField(models.CharField(max_length=128), blank=True, default=list)
 
 
 class VSVideo(models.Model):
     name = models.CharField(max_length=128)
     author = models.ForeignKey('VSUser', on_delete=models.CASCADE)
     published_date = models.DateTimeField(auto_now_add=True)
-    likes = ArrayField(models.CharField(max_length=128), blank=True, default=list())
+    likes = ArrayField(models.CharField(max_length=128), blank=True, default=list)
     description = models.TextField(blank=False, default='My new video')
     comments = models.ManyToManyField('VSComment', through='CommentVideo')
+    upload = models.FileField()
 
 
 class VSComment(models.Model):
     author = models.ForeignKey('VSUser', on_delete=models.CASCADE)
     text = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    likes = ArrayField(models.CharField(max_length=128), blank=True, default=list())
+    likes = ArrayField(models.CharField(max_length=128), blank=True, default=list)
+    ignored = models.BooleanField(default=False)
 
 
 class CommentVideo(models.Model):
